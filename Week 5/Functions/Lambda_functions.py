197137af184ae88f99ca2cbad74e445d6f838a7c
Tanushree <doddamanitanushree@gmail.com>
2025-11-17 18:17:37 +0530

    This is my file

---
+++ Week 5/Functions/Lambda_functions.py
@@ -0,0 +1,10 @@
+add = lambda x, y: x + y
+sub = lambda x, y: x - y
+mul = lambda x, y: x * y
+div = lambda x, y: x / y
+print (add(10,20))
+print (sub(10,20))
+print (mul(10,20))
+print (div(10,20))
+
+print(type(add))
