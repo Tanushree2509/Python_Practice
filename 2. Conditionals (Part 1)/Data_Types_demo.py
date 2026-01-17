023f79d3b03d775ebb9de379e20b5d378a7cc852
Tanushree <doddamanitanushree@gmail.com>
2026-01-17 12:04:52 +0530

    Update

---
+++ 2. Conditionals (Part 1)/Data_Types_demo.py
@@ -0,0 +1,26 @@
+#Type 1_demo.py
+n=10
+print(n)
+r=6.3
+print(r)
+t= 'tanu'
+print(t)
+print ("n is of type",type(n))
+print ("r is of type",type(r))
+print ("t is of type",type(t))
+
+#Type 2_demo.py
+#list_indexing_demo.py
+'''
+l=[1,2,3,4,5]
+print(l[2])
+print ("l is of type",type(l[2]))
+'''
+#Type 3_demo.py
+#Type casting_demo.py
+'''
+a=int (5.7)
+b= int('10')
+print(a, type(a))
+print(b, type(b))
+'''
