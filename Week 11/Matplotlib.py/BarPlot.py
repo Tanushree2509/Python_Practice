11ea4730f1fbbe8b50a003a831be3cda30b75c8e
Tanushree Doddamani <doddamanitanushree@gmail.com>
2026-01-17 12:19:11 +0530

    Delete Week 11 directory

--- Week 11/Matplotlib.py/BarPlot.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.array([1, 2, 3, 4, 5])
-y = np.array([10, 15, 13, 17, 20])
-plt.bar(x, y)
-plt.show()
--- Week 11/Pandas.py/Code.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-scores = pd.read_csv('scores.csv')
-print(scores[scores['Gender'] =='M']['Total'].max())
-print(scores[scores['Gender'] =='F']['Total'].min())
-print(scores)
