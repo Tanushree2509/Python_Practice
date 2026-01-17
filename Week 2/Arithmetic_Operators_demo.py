1d220ddbdca4d488b10fba457978372f0ae1b5de
Tanushree Doddamani <doddamanitanushree@gmail.com>
2026-01-17 12:20:35 +0530

    Delete Week 2 directory

--- Week 2/Arithmetic_Operators_demo.py
+++
@@ -1,7 +0,0 @@
-n=2 ** 3 ** 0 
-#The operator ** is right-associative, meaning it evaluates from right to left:
-#First: 3 ** 0 = 1 (any number raised to 0 equals 1)
-#Then: 2 ** 1 = 2
-print(n)
-i=25
-print(i%26)
--- Week 2/Even_Odd.py
+++
@@ -1,6 +0,0 @@
-h=int (input("Enter a number: "))
-
-if h%2==0:
-    print(h,"is even")
-else:
-    print(h,"is odd")
