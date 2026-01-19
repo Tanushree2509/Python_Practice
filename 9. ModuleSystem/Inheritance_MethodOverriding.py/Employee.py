875f304ba9343b047b23d8030b0910975a0b2fff
Tanushree Doddamani <doddamanitanushree@gmail.com>
2026-01-19 16:37:27 +0530

    Delete 9. ModuleSystem directory

--- 9. ModuleSystem/Inheritance_MethodOverriding.py/Employee.py
+++
@@ -1,8 +0,0 @@
-from Person import Person
-class Employee(Person):
-    def __init__(self, name, age, salary):
-        super().__init__(name, age) ##
-        self.salary = salary
-    def display(self):
-       # super().display() ##
-        print(self.name, self.age, self.salary)
