b29c3a6db63b9342528a18707b1f0e09a4b946b7
Tanushree <doddamanitanushree@gmail.com>
2026-03-28 09:46:36 +0530

    Change

---
+++ Python_Practice/2. Conditionals (Part 1)/ListConcept.py
@@ -0,0 +1,9 @@
+# list = [1,2 ,3]
+# print (list.append(4))
+# print (list.sort (reverse = True))
+# print (list)
+# list.remove(1)
+# print (list) #if duplicates are there then the 1st occurance is removed
+a, b, c = map(str, input("Enter three movies separated by spaces: ").split())
+list = [a,b,c]
+print(list)
